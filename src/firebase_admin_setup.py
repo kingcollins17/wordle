2980e7845de29e2613e5a9e690403674627bc2cf
Zide people <zidepeople@Zides-MacBook-Pro.local>
2025-09-23 15:22:53 -0700

    Remove service account from commit

--- src/firebase_admin_setup.py
+++ src/firebase_admin_setup.py
@@ -2,11 +2,13 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 
+from src.core.env import get_environment_or_default
+
 # Get current working directory
 cwd = os.getcwd()
 
 # Join with the filename
 service_account_path = os.path.join(cwd, "serviceAccountKey.json")
-
-cred = credentials.Certificate(service_account_path)
+environment = get_environment_or_default()
+cred = credentials.Certificate(environment.get_firebase_config())
 firebase_admin.initialize_app(cred)
